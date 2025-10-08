9bef67b0d6a3df445f6c3da519808b6230d732c1
VeselayaShishka <protomax040@gmail.com>
2025-10-08 17:14:22 +0300

    completed lab 1

---
+++ Lab1_insertSort_selectionSort/src/Main.java
@@ -0,0 +1,26 @@
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int[] arr = {50, 80, 19, 86, 35, 7, 60, 48, 51};
+
+        int[] arr2 = {50, 80, 19, 86, 35, 7, 60, 48, 51};
+
+        selectionSort.sort(arr);
+
+        System.out.println("\n\n");
+
+        insertSort.sort(arr2);
+    }
+
+
+    public static void print (int[] arr){
+        for (int i : arr) {
+            System.out.print(i + " ");
+        }
+    }
+}
+
+
---
+++ Lab1_insertSort_selectionSort/src/selectionSort.java
@@ -0,0 +1,32 @@
+public class selectionSort {
+
+    public static void sort(int[] arr) {
+
+        int ass = 0, comp = 0;
+
+        for(int i = 0; i < arr.length-1; i++) {
+
+            int min = i;
+            ass +=1;
+
+            for(int j = i + 1; j < (arr.length); j++) {
+                comp+=1;
+                if (arr[j]<arr[min]){
+                    min = j;
+                    ass+=1;
+                }
+            }
+
+            comp+=1;
+            if(min !=i) {
+                int tmp = arr[i];
+                arr[i] = arr[min];
+                arr[min] = tmp;
+                ass+=3;
+            }
+            Main.print(arr);
+            System.out.println("\nassignees:"+ass+" comparisons:"+comp);
+        }
+    }
+
+}
