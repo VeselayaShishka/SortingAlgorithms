824086478ae903b40b2c042dd68f8912f092a798
VeselayaShishka <protomax040@gmail.com>
2025-10-08 17:09:32 +0300

    added lab 1

---
+++ Lab1_insertSort_selectionSort/Lab1_insertSort_selectionSort/src/insertSort.java
@@ -0,0 +1,31 @@
+public class insertSort {
+
+    public static void sort(int[] arr) {
+        int ass = 0, comp = 0;
+
+        for (int i = 1; i < arr.length; i++) {
+
+            int key = arr[i];
+            ass += 1;
+
+            int j = i - 1;
+            ass++;
+
+            while (j >= 0 && key < arr[j]) {
+                comp += 1;
+
+                arr[j + 1] = arr[j];
+                ass += 1;
+
+                j--;
+                ass++;
+            }
+
+            arr[j + 1] = key;
+            ass += 1;
+
+            Main.print(arr);
+            System.out.println("\nassignees:" + ass + " comparisons:" + comp);
+        }
+    }
+}
